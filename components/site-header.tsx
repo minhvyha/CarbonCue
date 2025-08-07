d255d6578af52c5956ecb980cd27d1ab9356df04
Minh Vy ha <minhvy.ha@outlook.com>
2025-08-07 19:47:24 +1000

    fix(site-header): adjust mobile menu height for better visibility

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -110,7 +110,7 @@ export function SiteHeader() {
           mobileMenuOpen ? "fixed inset-0 z-50" : "hidden"
         )}
       >
-        <div className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-background px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <div className="fixed h-fit inset-y-0 right-0 z-50 w-full overflow-y-auto bg-background px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
             <Link href="/" className="-m-1.5 p-1.5">
               <span className="sr-only">CarbonCue</span>
